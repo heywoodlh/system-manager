bd936b9a7eafa5760a2405c9e922b8689e0062ef
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-01-14 23:12:58 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import sys
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='manager',
+      version='0.1.0',
+      description="Simple machine management",
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+        "Intended Audience :: Developers",
+        ],
+      url='https://github.com/heywoodlh/manager',
+      author='Spencer Heywood',
+      author_email='l.spencer.heywood@protonmail.com',
+      license='APACHE-2.0',
+      packages=['manager'],
+      scripts=['bin/manager'],
+      python_requires='>3.5.2',
+      install_requires=[
+          'distro'
+      ],
+      zip_safe=False)
